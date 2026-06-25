7ac5f1b11ce30edcafa248ea237078ae9f29cfc2
MaxMicheli <weneveraband@gmail.com>
2026-06-25 23:59:09 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'lista-spesa-v1';
+const FILES = ['index.html', 'manifest.json', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
